6f6e0fa274f0b0d9b293bbc10b02aabe6974dcfc
Marty Penner <martypenner@users.noreply.github.com>
2020-09-14 10:39:46 -0400

    feat: get skeleton home page working

---
+++ pages/_document.js
@@ -0,0 +1,113 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cardo:400,400i,700|Montserrat:200,400" />
+          <link rel="stylesheet" href="https://d2gt2lt628wbyx.cloudfront.net/poetry.css" />
+
+          <script
+            type="text/javascript"
+            charset="UTF-8"
+            src="https://maps.googleapis.com/maps-api-v3/api/js/42/4/common.js"></script>
+          <script
+            type="text/javascript"
+            charset="UTF-8"
+            src="https://maps.googleapis.com/maps-api-v3/api/js/42/4/util.js"></script>
+
+          <script src="https://use.typekit.net/yuq4sen.js" async></script>
+          <link
+            rel="apple-touch-icon"
+            sizes="57x57"
+            href="https://d1tntvpcrzvon2.cloudfront.net/vwassets/new/images/favicon/apple-touch-icon-57x57.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="60x60"
+            href="https://d1tntvpcrzvon2.cloudfront.net/vwassets/new/images/favicon/apple-touch-icon-60x60.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="72x72"
+            href="https://d1tntvpcrzvon2.cloudfront.net/vwassets/new/images/favicon/apple-touch-icon-72x72.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="76x76"
+            href="https://d1tntvpcrzvon2.cloudfront.net/vwassets/new/images/favicon/apple-touch-icon-76x76.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="114x114"
+            href="https://d1tntvpcrzvon2.cloudfront.net/vwassets/new/images/favicon/apple-touch-icon-114x114.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="120x120"
+            href="https://d1tntvpcrzvon2.cloudfront.net/vwassets/new/images/favicon/apple-touch-icon-120x120.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="144x144"
+            href="https://d1tntvpcrzvon2.cloudfront.net/vwassets/new/images/favicon/apple-touch-icon-144x144.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="https://d1tntvpcrzvon2.cloudfront.net/vwassets/new/images/favicon/apple-touch-icon-152x152.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="https://d1tntvpcrzvon2.cloudfront.net/vwassets/new/images/favicon/apple-touch-icon-180x180.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href="https://d1tntvpcrzvon2.cloudfront.net/vwassets/new/images/favicon/favicon-32x32.png"
+            sizes="32x32"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href="https://d1tntvpcrzvon2.cloudfront.net/vwassets/new/images/favicon/android-chrome-192x192.png"
+            sizes="192x192"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href="https://d1tntvpcrzvon2.cloudfront.net/vwassets/new/images/favicon/favicon-16x16.png"
+            sizes="16x16"
+          />
+          <link rel="manifest" href="https://d1tntvpcrzvon2.cloudfront.net/vwassets/new/images/favicon/manifest.json" />
+          <link
+            rel="mask-icon"
+            href="https://d1tntvpcrzvon2.cloudfront.net/vwassets/new/images/favicon/safari-pinned-tab.svg"
+            color="#01b4c0"
+          />
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta
+            name="msapplication-TileImage"
+            content="https://d1tntvpcrzvon2.cloudfront.net/vwassets/new/images/favicon/mstile-144x144.png"
+          />
+          <meta name="theme-color" content="#ffffff" />
+
+          <link
+            href="https://d1tntvpcrzvon2.cloudfront.net/vwassets/new/static/css/main.0611cfee.css"
+            rel="stylesheet"
+          />
+          <link href="https://d1tntvpcrzvon2.cloudfront.net/vwassets/new/main.11e53fbf.css" rel="stylesheet" />
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- pages/index.js
+++ pages/index.js
@@ -1,56 +1,285 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div id="root">
       <Head>
-        <title>Create Next App</title>
+        <title>Marty Penner and Meaghan Jones</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href="https://github.com/vercel/next.js/tree/master/examples" className={styles.card}>
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}>
-            <h3>Deploy &rarr;</h3>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
+      <div id="zola-wedding-container">
+        <div>
+          <div id="public-wrapper">
+            <div className="website-title__cart noMobile"></div>
+            <div>
+              <div className="sticky-nav" style={{ transform: 'translateZ(0px)' }}>
+                <header id="public-page-header" className="clearfix">
+                  <div id="public-page-details" className="top-row hidden-xs hidden-sm">
+                    <h5 className="wedding-date">October 4, 2020</h5>
+                    <h5 className="wedding-location">Parry Sound, Ontario</h5>
+                  </div>
+                  <div className="col-xs-12 website-title">
+                    <h1 id="public-page-title" className="hidden visible-md visible-lg">
+                      <a href="/">Marty &amp; Meaghan</a>
+                    </h1>
+                    <div className="visible-sm visible-xs">
+                      <div className="nav-control zolaicon-public zolaicon-public-hamburger-menu" role="button"></div>
+                    </div>
+                    <div className="pages-nav-container">
+                      <div className="nav-container clearfix">
+                        <ul className="list-inline">
+                          <li>
+                            <a href="/" className="active">
+                              Home
+                            </a>
+                          </li>
+                          <li>
+                            <a href="/event">Schedule</a>
+                          </li>
+                          <li>
+                            <a href="/travel">Travel</a>
+                          </li>
+                          <li>
+                            <a href="/photo">Photos</a>
+                          </li>
+                          <li>
+                            <a href="/poi">Things To Do</a>
+                          </li>
+                          <li>
+                            <a href="/faq">FAQs</a>
+                          </li>
+                          <li>
+                            <a href="/rsvp">RSVP</a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                    <h1 className="hidden visible-xs visible-sm">
+                      <a href="/">Marty &amp; Meaghan</a>
+                    </h1>
+                    <div className="website-title__cart hidden-md hidden-lg"></div>
+                  </div>
+                </header>
+              </div>
+            </div>
+            <div id="public-page-content-wrapper" className="show-page">
+              <div>
+                <div className="public-page">
+                  <div>
+                    <div className="hero-wrapper">
+                      <div
+                        className="jumbotron home-page-hero has-image"
+                        style={{
+                          backgroundImage: "url('https://images.zola.com/38fcbdcf-4a82-4666-b0a2-7dc153ea0b58?h=700')",
+                        }}>
+                        <div className="hero-accent-top-left"></div>
+                        <div className="hero-accent-top-right"></div>
+                        <div className="hero-accent-bottom-left"></div>
+                        <div className="hero-accent-bottom-right"></div>
+                        <div className="homepage-header-text">
+                          <h1>Celebrate with us!</h1>
+                        </div>
+                        <div className="down-arrow">
+                          <i className="zolaicon-public zolaicon-public-chevron-down" role="button"></i>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="page-content has-entities has-image has-welcome-message">
+                      <div className="page-accent-top-left"></div>
+                      <div className="page-accent-top-right"></div>
+                      <div className="page-accent-bottom-left"></div>
+                      <div className="page-accent-bottom-right"></div>
+                      <div className="homepage-seal top-seal">
+                        <div className="seal-section">
+                          <span className="name-1">
+                            <span className="initial-1">M</span>
+                            <span className="initial-2">P</span>
+                          </span>
+                          <span>
+                            <span className="amp">&amp;</span>
+                            <span className="and">and</span>
+                            <span className="plus">+</span>
+                          </span>
+                          <span className="name-2">
+                            <span className="initial-1">M</span>
+                            <span className="initial-2">J</span>
+                          </span>
+                        </div>
+                      </div>
+                      <div className="names-section">
+                        <div className="names-accent-top-left"></div>
+                        <div className="names-accent-top-right"></div>
+                        <div className="names-accent-bottom-left"></div>
+                        <div className="names-accent-bottom-right"></div>
+                        <div className="name-section-left">
+                          <h2 className="partner-1-name">
+                            <span className="first-name">
+                              <span className="first-initial">M</span>
+                              <span className="full-first-name">arty</span>
+                            </span>
+                            <span className="last-name">
+                              <span className="last-initial"> P</span>
+                              <span className="full-last-name">enner</span>
+                            </span>
+                          </h2>
+                        </div>
+                        <div className="name-section-middle">
+                          <div className="content">
+                            <h1 className="name-1">
+                              <span className="first-name">
+                                <span className="first-initial">M</span>
+                                <span className="full-first-name">arty</span>
+                              </span>
+                              <span className="last-name">
+                                <span className="last-initial"> P</span>
+                                <span className="full-last-name">enner</span>
+                              </span>
+                            </h1>
+                            <span className="amp">&amp;</span>
+                            <span className="and">and</span>
+                            <span className="plus">+</span>
+                            <h1 className="name-2">
+                              <span className="first-name">
+                                <span className="first-initial">M</span>
+                                <span className="full-first-name">eaghan</span>
+                              </span>
+                              <span className="last-name">
+                                <span className="last-initial"> J</span>
+                                <span className="full-last-name">ones</span>
+                              </span>
+                            </h1>
+                          </div>
+                        </div>
+                        <div className="name-section-right">
+                          <h2 className="partner-2-name">
+                            <span className="first-name">
+                              <span className="first-initial">M</span>
+                              <span className="full-first-name">eaghan</span>
+                            </span>
+                            <span className="last-name">
+                              <span className="last-initial"> J</span>
+                              <span className="full-last-name">ones</span>
+                            </span>
+                          </h2>
+                        </div>
+                      </div>
+                      <div className="wedding-info-section">
+                        <div className="horizontal">
+                          <div className="content">
+                            <div className="location">
+                              <h4 className="in">in</h4>
+                              <h3>
+                                <span className="city">Parry Sound</span>
+                                <span className="comma">, </span>
+                                <span className="state">Ontario</span>
+                              </h3>
+                            </div>
+                            <div className="date">
+                              <span>
+                                <h4 className="on">on</h4>
+                                <h3>
+                                  <span className="month">October</span>
+                                  <span className="day">4</span>
+                                  <span className="year">2020</span>
+                                </h3>
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="vertical-single">
+                          <div className="name-1 hidden-xs">
+                            <div className="initials">
+                              <span className="initial-1">M</span>
+                              <div className="accent"></div>
+                              <span className="initial-2">P</span>
+                            </div>
+                          </div>
+                          <div className="info-section">
+                            <div className="content">
+                              <div className="date-wrapper">
+                                <h3 className="date">
+                                  <div className="month">
+                                    <span className="numeric">10</span>
+                                    <span className="word">October</span>
+                                  </div>
+                                  <span className="day">4</span>
+                                  <div className="year">
+                                    <span className="full">2020</span>
+                                    <span className="minimal">20</span>
+                                  </div>
+                                </h3>
+                              </div>
+                              <h4 className="location">Parry Sound, Ontario</h4>
+                            </div>
+                          </div>
+                          <div className="name-2 hidden-xs">
+                            <div className="initials">
+                              <span className="initial-1">M</span>
+                              <div className="accent"></div>
+                              <span className="initial-2">J</span>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="vertical-double">
+                          <div className="wedding-info-column wedding-info-left">
+                            <h4 className="location">Parry Sound, Ontario</h4>
+                            <span className="big-initial">M</span>
+                            <h4 className="and">and</h4>
+                            <span className="big-initial">M</span>
+                          </div>
+                          <div className="wedding-info-column wedding-info-right">
+                            <h4 className="day-month">Sunday</h4>
+                            <span className="big-initial day">10</span>
+                            <h4>/</h4>
+                            <span className="big-initial month">4</span>
+                            <h4>2020</h4>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="entity-sections homepage-sections clearfix">
+                        <div className="entity-section homepage-section clearfix">
+                          <div className="entity-section-title homepage-section-title">
+                            <h2 className="section-title">How we met</h2>
+                            <h4 className="section-subtitle">First comes school, then comes marriage</h4>
+                          </div>
+                          <div className="entity-section-description homepage-section-description-v2">
+                            <p>Babies, weddings, such and so on</p>
+                          </div>
+                          <div className="section-accent"></div>
+                        </div>
+                      </div>
+                      <div className="homepage-seal bottom-seal">
+                        <div className="seal-section">
+                          <span className="name-1">
+                            <span className="initial-1">M</span>
+                            <span className="initial-2">P</span>
+                          </span>
+                          <span>
+                            <span className="amp">&amp;</span>
+                            <span className="and">and</span>
+                            <span className="plus">+</span>
+                          </span>
+                          <span className="name-2">
+                            <span className="initial-1">M</span>
+                            <span className="initial-2">J</span>
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                    <span>
+                      <div
+                        className="bottom-section"
+                        style={{
+                          backgroundImage: "url('https://images.zola.com/52657661-692c-4068-952f-f1213d683b43?h=700')",
+                        }}></div>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer">
-          Powered by <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
