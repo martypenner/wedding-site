84625c29231ad1db79162ccb9056034cbcd68f45
Marty Penner <penner.marty@gmail.com>
2021-08-09 19:22:12 -0400

    chore: update deps and add autoformatting

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  purge: ["./src/**/*.html", "./src/**/*.js", "./src/**/*.ts", "./src/**/.tsx"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Cardo"].concat(defaultTheme.fontFamily.sans),
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
