9d2bdffcfbf677e3d9c4543dbe4bba0fe6f89f37
Marty Penner <penner.marty@gmail.com>
2021-08-18 19:29:37 -0400

    chore: remove unneeded key in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,6 @@ module.exports = {
         cardo: ['Cardo'].concat(defaultTheme.fontFamily.sans),
         montserrat: ['Montserrat'].concat(defaultTheme.fontFamily.sans),
       },
-      colors: {},
       spacing: {
         125: '31.25rem',
         150: '37.5rem',
