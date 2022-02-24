0990f3cabc0e8f4cc7be84e7cacfe940adfb5b8f
Marty Penner <penner.marty@gmail.com>
2022-02-24 09:06:04 -0500

    chore: add xstate inspector

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,11 @@
 import '../styles/app.css';
 
+if (process.env.NODE_ENV === 'development' && typeof window !== 'undefined') {
+  require('@xstate/inspect').inspect({
+    iframe: false,
+  });
+}
+
 function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
