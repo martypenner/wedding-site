179be5f6319be2e58b34f7b75e2ff67eb4f8495d
Marty Penner <penner.marty@gmail.com>
2022-03-02 21:27:06 -0500

    feat: begin migrating towards airtable format

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,67 +1,62 @@
-import { PrismaClient } from '@prisma/client';
+import { AllowedEvents, PrismaClient } from '@prisma/client';
+import attendees from './data.json';
+
 const prisma = new PrismaClient();
 
 async function main() {
-	const theoName = 'Theo Boniferous';
-	const theo = await prisma.attendee.upsert({
-		where: {
-			name: theoName,
-		},
-		update: {},
-		create: {
-			name: theoName,
-			allowedEvents: ['CEREMONY', 'RECEPTION'],
-			partyMembers: {
-				create: [
-					{
-						name: 'Barbara Pines',
-						allowedEvents: ['CEREMONY'],
-					},
-					{
-						name: 'Jimmy Elm',
-						allowedEvents: ['RECEPTION'],
-						attendanceAnswer: {
+	for (const attendee of attendees) {
+		const allowedEvents = attendee['Invited Events']
+			.split(',')
+			.map((event) => event.toUpperCase()) as AllowedEvents[];
+
+		// Grab all party members from the "plus one" up to "additional guest 6".
+		const partyMembers = [attendee['Plus One']].concat(
+			...Array(6)
+				.fill(null)
+				.map((_, index) => attendee[`Additional Guest ${index + 1}`])
+				.filter((name) => name.trim().length > 0)
+		);
+
+		const attendeeUpsertResponse = await prisma.attendee.upsert({
+			where: {
+				name: attendee.Guest,
+			},
+			update: {},
+			create: {
+				name: attendee.Guest,
+				allowedEvents,
+				partyMembers: {
+					create: partyMembers.map((partyMember) => ({
+						name: partyMember,
+						allowedEvents,
+					})),
+				},
+				attendanceAnswer: {
+					create: {
+						ceremony: {
 							create: {
-								ceremony: {
-									create: {
-										willAttend: false,
-									},
-								},
-								reception: {
-									create: {
-										willAttend: false,
-									},
-								},
+								willAttend: true,
 							},
 						},
-					},
-				],
-			},
-			attendanceAnswer: {
-				create: {
-					ceremony: {
-						create: {
-							willAttend: true,
-						},
-					},
-					reception: {
-						create: {
-							willAttend: true,
-							dietaryRestrictions: 'seeds',
-							tuneThatWillMakeYouBoogie: 'Cat Stevens',
+						reception: {
+							create: {
+								willAttend: true,
+								dietaryRestrictions: 'seeds',
+								tuneThatWillMakeYouBoogie: 'Cat Stevens',
+							},
 						},
 					},
 				},
 			},
-		},
-		include: {
-			attendanceAnswer: true,
-			partyMembers: true,
-			partyMembersThatIncludedMe: true,
-		},
-	});
+			include: {
+				attendanceAnswer: true,
+				partyMembers: true,
+				partyMembersThatIncludedMe: true,
+			},
+		});
 
-	console.dir({ theo });
+		console.dir({ attendeeUpsertResponse }, { depth: null });
+	}
 }
 
 main()
