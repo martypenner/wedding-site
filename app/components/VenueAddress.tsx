3833426dd8cc1abdac07f44755264d52456e0545
Marty Penner <martypenner@users.noreply.github.com>
2022-09-04 17:12:38 +0000

    feat: add directions to address

--- app/components/VenueAddress.tsx
+++ app/components/VenueAddress.tsx
@@ -1,13 +1,22 @@
+import { Fragment } from 'react';
+
 export default function VenueAddress() {
 	return (
-		<address className="mb-8 font-semibold not-italic">
-			Seguin Valley Golf Club
-			<br />
-			144 Badger Road
-			<br />
-			Parry Sound, ON P2A 2W8
-			<br />
-			Canada
-		</address>
+		<Fragment>
+			<address className="mb-8 font-semibold not-italic">
+				Seguin Valley Golf Club
+				<br />
+				144 Badger Road
+				<br />
+				Parry Sound, ON P2A 2W8
+				<br />
+				Canada
+			</address>
+
+			<p className="mb-8">
+				Take exit 217 off of Hwy 400 towards Badger Road. Turn North onto Badger
+				Road. The golf club will then appear on your left after 2 km.
+			</p>
+		</Fragment>
 	);
 }
